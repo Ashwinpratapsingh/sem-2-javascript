8b2b3edcd26e4006396b26c1a6661ee903de35a5
Ashwinpratapsingh <apsingh312007@gmail.com>
2026-03-16 15:30:54 +0530

    learning dom and function

---
+++ dom.js
@@ -0,0 +1,8 @@
+function show(){
+    let name=document.getElementById("myName").vaueOf;
+    alert(name)
+}
+//dom 11th march 26
+function changecolor(){
+    document.getElementById('text').style.color"red";
+}
